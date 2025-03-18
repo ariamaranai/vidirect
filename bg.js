9189453108d8a655fc26072c8eeee79876a43540
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-03-18 18:42:09 +0900

    Add files via upload

---
+++ bg.js
@@ -0,0 +1,34 @@
+{
+  let run = (a, b) => {
+    let tabId = (b || a).id;
+    chrome.scripting.executeScript({
+      target: b ? { tabId, frameIds: [a.frameId] } : { tabId, allFrames: !0 },
+      func: () => {
+        let video = document.body.getElementsByTagName("video");
+        let i = video.length;
+        let index = 0;
+        if (i) {
+          if (i > 1) {
+            let maxWidth = 0;
+            let width = 0;
+            while (
+              maxWidth < (width = video[--i].offsetWidth) && (maxWidth = width, index = i),
+              i
+            );
+          }
+          (video = video[index].src) && video[0] == "h" && open(video);
+        }
+      }
+    }).catch(() => 0);
+  }
+  chrome.action.onClicked.addListener(run);
+  chrome.contextMenus.onClicked.addListener(run);
+}
+chrome.runtime.onInstalled.addListener(() =>
+  chrome.contextMenus.create({
+    id: "",
+    title: "Open video in new tab",
+    contexts: ["page", "video"],
+    documentUrlPatterns: ["https://*/*", "https://*/"]
+  })
+);
