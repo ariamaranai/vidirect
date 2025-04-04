ecf3fca57229a7ee2cbe58a905e9da5da617650d
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-04-04 13:45:51 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,24 +1,25 @@
 {
   let run = (a, b) => {
     let tabId = (b || a).id;
-    chrome.scripting.executeScript({
+    chrome.userScripts.execute({
       target: b ? { tabId, frameIds: [a.frameId] } : { tabId, allFrames: !0 },
-      func: () => {
-        let video = document.body.getElementsByTagName("video");
-        let i = video.length;
-        let index = 0;
-        if (i) {
-          if (i > 1) {
-            let maxWidth = 0;
-            let width = 0;
-            while (
-              maxWidth < (width = video[--i].offsetWidth) && (maxWidth = width, index = i),
-              i
-            );
-          }
-          (video = video[index].currentSrc) && video[0] == "h" && open(video);
-        }
-      }
+      js: [{ code:
+`{
+let video = document.body.getElementsByTagName("video");
+let i = video.length;
+let index = 0;
+if (i) {
+  if (i > 1) {
+    let maxWidth = 0;
+    let width = 0;
+    while (
+      maxWidth < (width = video[--i].offsetWidth) && (maxWidth = width, index = i),
+      i
+    );
+  }
+  (video = video[index].currentSrc) && video[0] == "h" && video.pause(open(video));
+}
+}`    }]
     }).catch(() => 0);
   }
   chrome.action.onClicked.addListener(run);
