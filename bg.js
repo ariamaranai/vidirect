67ab5f86312613349a0a579b66c1170d6c16b95f
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-05-01 09:46:10 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -5,18 +5,18 @@
       target: b ? { tabId, frameIds: [a.frameId] } : { tabId, allFrames: !0 },
       js: [{ code:
 `{
-let video = document.body.getElementsByTagName("video");
-let i = video.length;
-if (i) {
-  let index = 0;
-  let maxWidth = 0;
-  let width = 0;
-  while (
-    maxWidth < (width = video[--i].offsetWidth) && (maxWidth = width, index = i),
-    i
-  );
-  (video = video[index]) && video.pause(open(video.currentSrc));
-}
+  let video = document.body.getElementsByTagName("video");
+  let i = video.length;
+  if (i) {
+    let index = 0;
+    let maxWidth = 0;
+    let width = 0;
+    while (
+      maxWidth < (width = video[--i].offsetWidth) && (maxWidth = width, index = i),
+      i
+    );
+    (video = video[index]) && video.pause(open(video.currentSrc));
+  }
 }`
       }]
     }).catch(() => 0);
