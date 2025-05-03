8c2472067850bc1ebe4f508f5a33ec6b7ec2d2ab
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-05-04 00:38:00 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -4,7 +4,7 @@
     chrome.userScripts.execute({
       target: b ? { tabId, frameIds: [a.frameId] } : { tabId, allFrames: !0 },
       js: [{ code:
-`{
+`(() => {
   let video = document.body.getElementsByTagName("video");
   let i = video.length;
   if (i) {
@@ -15,22 +15,34 @@
       maxWidth < (width = video[--i].offsetWidth) && (maxWidth = width, index = i),
       i
     );
-    (video = video[index]) &&
-    (i = video.currentSrc)[0] == "h" &&
-    video.pause(open(i));
+    return (video = video[index]) && (i = video.currentSrc)[0] == "h" && (video.pause(), i);
   }
-}`
+})();`
       }]
-    }).catch(() => 0);
+    }).then(results =>
+      (results &&= results[0].result) &&
+      chrome.tabs.create({
+        url: results,
+        index: (b || a).index + 1
+      }, tab =>
+        chrome.management.getAll(crx =>
+          (crx = crx.find(v => v.name == "kbdvid")) &&
+          chrome.runtime.sendMessage(crx.id, tab.id)
+        )
+      )
+    ).catch(() => 0);
   }
   chrome.action.onClicked.addListener(run);
   chrome.contextMenus.onClicked.addListener(run);
 }
-chrome.runtime.onInstalled.addListener(() =>
+chrome.runtime.onInstalled.addListener(() => (
   chrome.contextMenus.create({
     id: "",
     title: "Open video in new tab",
     contexts: ["page", "video"],
     documentUrlPatterns: ["https://*/*"]
+  }),
+  chrome.userScripts.configureWorld({
+    messaging: !0
   })
-);
+));
