ff137be08762d784016d72fd8c9a886ef9d9a06f
a <106607480+ariamaranai@users.noreply.github.com>
2025-05-22 19:06:22 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -2,13 +2,9 @@
   let run = (a, b) => {
     let tabId = (b || a).id;
     let frameId = b && a.frameId;
-    chrome.management.getAll(crx => {
-      let f = tab => tab.url || tab.index - (b || a).index - 1 || chrome.runtime.sendMessage(crx.id, tab.id);
-
-      (crx = crx.find(v => v.name == "kbdvid")) && chrome.tabs.onCreated.addListener(f);
-      chrome.userScripts.execute({
-        target: frameId ? { tabId, frameIds: [frameId] } : { tabId, allFrames: !0 },
-        js: [{ code:
+    chrome.userScripts.execute({
+      target: frameId ? { tabId, frameIds: [frameId] } : { tabId, allFrames: !0 },
+      js: [{ code:
 `{
   let video = document.body.getElementsByTagName("video");
   let i = video.length;
@@ -23,10 +19,8 @@
     (video = video[index]) && (i = video.currentSrc)[0] == "h" && video.pause(open(i));
   }
 }`
-        }]
-      }).catch(() => 0);
-      crx && chrome.tabs.onCreated.removeListener(f);
-    });
+      }]
+    }).catch(() => 0);
   }
   chrome.action.onClicked.addListener(run);
   chrome.contextMenus.onClicked.addListener(run);
