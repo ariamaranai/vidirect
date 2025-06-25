9defe1a99bf4fafd7990b58dbc8c58106355173c
a <106607480+ariamaranai@users.noreply.github.com>
2025-06-26 00:24:53 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,9 +1,12 @@
 {
-  let run = (a, b) =>
-    chrome.userScripts.execute({
-      target: { tabId: (b || a).id, allFrames: !0 },
-      js: [{ file: "video.js" }]
-    }).catch(() => 0);
+  let run = async (a, b) => {
+    try {
+      await chrome.userScripts.execute({
+        target: { tabId: (b || a).id, allFrames: !0 },
+        js: [{ file: "video.js" }]
+      });
+    } catch (e) {}
+  }
   chrome.action.onClicked.addListener(run);
   chrome.contextMenus.onClicked.addListener(run);
 }
