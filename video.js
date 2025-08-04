c95751346544398773aa7023f4b669592b034946
a <106607480+ariamaranai@users.noreply.github.com>
2025-08-04 09:31:41 +0900

    Add files via upload

--- video.js
+++ video.js
@@ -1,16 +1,23 @@
 {
-  let video = document.body.getElementsByTagName("video");
-  let i = video.length;
-  if (i) {
-    let index = 0;
-    let maxWidth = 0;
-    let width = 0;
-    while (
-      video[--i].readyState &&
-      maxWidth < (width = video[i].offsetWidth) &&
-      (maxWidth = width, index = i),
-      i
-    );
-    (video = video[index]) && (i = video.currentSrc)[0] == "h" && video.pause(open(i));
+  let d = document;
+  let { scrollingElement } = d; 
+  let cx = (innerWidth + scrollingElement.scrollLeft) / 2;
+  let cy = (innerHeight + scrollingElement.scrollTop) / 2; 
+  let videos = d.getElementsByTagName("video");
+  let video;
+  let minds = 2e9;
+  let i = 0;
+  while (i < videos.length) {
+    let _video = videos[i];
+    if (_video.readyState && _video.currentSrc[0] == "h") {
+      let rect = _video.getBoundingClientRect();
+      let ds = Math.abs(cx - (rect.width / 2 + rect.x)) + Math.abs(cy - (rect.height / 2 + rect.y));
+      ds < minds && (
+        minds = ds,
+        video = _video
+      );
+    }
+    ++i;
   }
+  video && video.pause(open(video.currentSrc));
 }
