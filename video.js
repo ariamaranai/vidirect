f4a94aa5ee65e8d19c32f63468b02b41742c2c06
a <106607480+ariamaranai@users.noreply.github.com>
2025-08-09 02:12:04 +0900

    Add files via upload

--- video.js
+++ video.js
@@ -1,15 +1,15 @@
 {
-  let d = document;
-  let videos = d.getElementsByTagName("video");
+  let { Math, document, innerWidth, innerHeight } = self;
+  let { max, min } = Math;
+  let videos = document.getElementsByTagName("video");
   let video;
-  let { innerWidth, innerHeight } = self;
   let maxVisibleSize = 0;
   let i = 0;
   while (i < videos.length) {
     let _video = videos[i];
     if (_video.readyState && _video.currentSrc[0] == "h") {
       let { x, right, y, bottom } = _video.getBoundingClientRect();
-      let visibleSize = Math.max(Math.min(right, innerWidth) - Math.max(x, 0), 0) * Math.max(Math.min(bottom, innerHeight) - Math.max(y, 0), 0);
+      let visibleSize = max(min(right, innerWidth) - max(x, 0), 0) * max(min(bottom, innerHeight) - max(y, 0), 0);
       maxVisibleSize < visibleSize && (
         maxVisibleSize = visibleSize,
         video = _video
