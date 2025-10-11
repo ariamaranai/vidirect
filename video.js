0db926907b7d33ef29d7eb7464435c8efb4dc3ba
a <106607480+ariamaranai@users.noreply.github.com>
2025-10-11 16:58:47 +0900

    Add files via upload

--- video.js
+++ video.js
@@ -2,21 +2,26 @@
   let { Math, document, innerWidth, innerHeight } = self;
   let { max, min } = Math;
   let { fullscreenElement } = document;
-  let videos = (fullscreenElement ?? document).getElementsByTagName("video");
-  let video;
-  let maxVisibleSize = 0;
-  let i = 0;
-  while (i < videos.length) {
-    let _video = videos[i];
-    if (_video.readyState && _video.currentSrc[0] == "h") {
-      let { x, right, y, bottom } = _video.getBoundingClientRect();
-      let visibleSize = max(min(right, innerWidth) - max(x, 0), 0) * max(min(bottom, innerHeight) - max(y, 0), 0);
-      maxVisibleSize < visibleSize && (
-        maxVisibleSize = visibleSize,
-        video = _video
-      );
+  let video = fullscreenElement;
+  if (video?.tagName != "VIDEO") {
+    let videos = (fullscreenElement ?? document).getElementsByTagName("VIDEO");
+    let maxVisibleSize = 0;
+    let i = 0;
+    while (i < videos.length) {
+      let _video = videos[i];
+      if (_video.readyState && _video.currentSrc[0] == "h") {
+        let { x, right, y, bottom } = _video.getBoundingClientRect();
+        let visibleSize = max(min(right, innerWidth) - max(x, 0), 0) * max(min(bottom, innerHeight) - max(y, 0), 0);
+        maxVisibleSize < visibleSize && (
+          maxVisibleSize = visibleSize,
+          video = _video
+        );
+      }
+      ++i;
     }
-    ++i;
-  }
-  (video ??= fullscreenElement?.shadowRoot?.querySelector("video")) && video.pause(open(video.currentSrc));
+    video ?? ((video = fullscreenElement?.shadowRoot?.querySelector("VIDEO")) && !video.readyState || (video = 0));
+  } else
+    video.readyState || (video = 0);
+
+  video && video.pause(open(video.currentSrc));
 }
